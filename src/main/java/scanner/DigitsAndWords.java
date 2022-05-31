b4981b07f11f12a5ba7a312153b441d5c19aad1d
AlenaAgeeva <alenapvageeva@gmail.com>
2022-05-31 18:31:10 -0300

    2.3 scanner DigitsAndWords

---
+++ src/main/java/scanner/DigitsAndWords.java
@@ -0,0 +1,26 @@
+package scanner;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * The user enters a list of words (and numbers) from the keyboard.
+ * Output words in ascending order, numbers in descending order.
+ * @author Alena Ageeva
+ */
+public class DigitsAndWords {
+    public static void main(String[] args) throws Exception {
+        List<String> lines = new Scanner(System.in)
+                .tokens()
+                .takeWhile(x -> !x.equals("exit")).collect(Collectors.toList());
+        lines.stream()
+                .filter(x -> x.matches("-?\\d+(.\\d+)?"))
+                .sorted(Comparator.reverseOrder())
+                .forEach(s -> System.out.print(s+" "));
+        System.out.println();
+        lines.stream()
+                .filter(x -> !x.matches("-?\\d+(.\\d+)?"))
+                .sorted(String::compareTo)
+                .forEach(s -> System.out.print(s+" "));
+    }
+}
